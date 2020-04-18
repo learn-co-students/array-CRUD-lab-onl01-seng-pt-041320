ad6decf430e81e94f7aa2a16d25d6d6605b1f322
guerino antonini <guerantonini94@gmail.com>
2020-04-18 18:53:56 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  empty_array_games = []
 end
 
 def create_an_array
-  
+  array_of_letters = ["a", "b", "c", "d"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array_of_letters = ["a", "b", "c", "d"]
+    array_of_letters.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array_of_letters = ["a", "b", "c", "d"]
+  array_of_letters.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array_of_letters = ["a", "b", "c", "d", "arrays!"]
+    array_of_letters.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+   array_of_letters = ["wow","a", "b", "c", "d",]
+   array_of_letters.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[5]
 end
