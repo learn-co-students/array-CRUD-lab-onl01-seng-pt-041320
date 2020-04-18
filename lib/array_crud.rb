f3696bd844164ac550aa0e1d9671aa4d8a2812cb
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-18 14:29:21 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  ["vanilla", "chocolate", "strawberry", "oreo"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(ice_cream, strawberry)
+  ice_cream << strawberry
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(ice_cream, strawberry)
+  ice_cream.unshift(strawberry)
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(ice_cream)
+  ice_cream.pop
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(ice_cream)
+  ice_cream.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(ice_cream)
+  ice_cream[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(ice_cream)
+  x = ice_cream.count
+  ice_cream[x-1]
 end
